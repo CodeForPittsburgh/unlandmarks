ac65906d897a4c6510e3951d4a18bfeda465983b
mhowe0422 <mhowe0422@msn.com>
2017-12-17 15:18:14 -0500

    REMOVED

--- code/python/unlandmark_main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python2
-#encoding: UTF-8
-
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-if __name__ == "__main__":
-    print "Unlandmark"
